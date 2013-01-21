027a848d8d4373f0333d69b548909188e501024b
Dmitry Sviatozevsky <ua.fantomas@gmail.com>
2013-01-21 15:58:02 +0200

    Устранена проблема с кодировкой

--- engine/get_weather.php
+++ engine/get_weather.php
@@ -12,6 +12,7 @@
 			$cuurTime=date('Y-m-d H:s:y');
 
 			mysql_connect("localhost","root","toor") OR DIE("Не могу создать соединение ");
+			mysql_set_charset("utf8");
 			mysql_select_db("weather") or die(mysql_error());
 			$query = "INSERT INTO weather_bouth (temp,weather,time)
 			VALUES('$temp','$w', '$cuurTime')"; 
